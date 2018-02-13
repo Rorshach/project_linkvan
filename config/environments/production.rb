74de6786d0a6d283e1efb2192113df22a495d1ff
Rorshach <f.tso11200@gmail.com>
2018-02-12 22:27:36 -0800

    Testing with different config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            ENV['gmail_username'],
-  password:             ENV['gmail_password'],
+  user_name:            'linkvanproject@gmail.com',
+  password:             '4linkvananalytics',
   authentication:       'plain',
   enable_starttls_auto: true  }
 end
