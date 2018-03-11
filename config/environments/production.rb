ac62b5725672d2cf45ac17f5ac2c34e667ee6a14
Rorshach <f.tso11200@gmail.com>
2018-03-10 21:27:35 -0800

    Changed port number back.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address =>              'smtp.gmail.com',
-  :port =>                 587,
+  :port =>                 '587',
   :domain =>              'myapp.herokuapp.com',
   :user_name =>            ENV['GMAIL_USERNAME'],
   :password =>             ENV['GMAIL_PASSWORD'],
