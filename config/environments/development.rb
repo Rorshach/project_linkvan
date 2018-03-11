c0123731e50dd58c2b6c113abe4861a156fd2f97
Rorshach <f.tso11200@gmail.com>
2018-03-10 16:17:44 -0800

    Testing mailer on heroku.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ config.action_mailer.smtp_settings = {
     :address =>              'smtp.gmail.com',
     :port =>                 '587',
     :domain =>              'gmail.com',
-    :user_name =>            APP_CONFIG['gmail_username'],
-    :password =>             APP_CONFIG['gmail_password'],
+    :user_name =>            ENV['gmail_username'],
+    :password =>             ENV['gmail_password'],
     :authentication =>       :plain,
     :enable_starttls_auto => true  }
 end
