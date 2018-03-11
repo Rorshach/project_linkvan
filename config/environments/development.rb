fbc6e5a019ece9d15ffd7484bd71380f6875ff37
Rorshach <f.tso11200@gmail.com>
2018-03-10 21:22:23 -0800

    Changed port and debugging.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Linkvan::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
@@ -38,7 +38,7 @@ config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     :address =>              'smtp.gmail.com',
-    :port =>                 '587',
+    :port =>                 587,
     :domain =>              'myapp.herokuapp.com',
     :user_name =>            ENV['GMAIL_USERNAME'],
     :password =>             ENV['GMAIL_PASSWORD'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Linkvan::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address =>              'smtp.gmail.com',
-  :port =>                 '587',
+  :port =>                 587,
   :domain =>              'myapp.herokuapp.com',
   :user_name =>            ENV['GMAIL_USERNAME'],
   :password =>             ENV['GMAIL_PASSWORD'],
