7310d41a6cbbfb5f44539568637b903cc4c791ff
Rorshach <f.tso11200@gmail.com>
2018-03-10 16:39:07 -0800

    Changed host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Linkvan::Application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-config.action_mailer.default_url_options = { :host => '24.87.49.179:3000' }
+config.action_mailer.default_url_options = { :host => 'interstellar500.herokuapp.com/' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     :address =>              'smtp.gmail.com',
