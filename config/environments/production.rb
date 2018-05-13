7628fb481801d9a225dbd434056bcf693741dfa7
Rorshach <f.tso11200@gmail.com>
2018-05-12 19:17:08 -0700

    Changed host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Linkvan::Application.configure do
 
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'linkvan2.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address =>              'smtp.gmail.com',
